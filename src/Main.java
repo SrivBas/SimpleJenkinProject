9a562112f1c63eac0f7fbeb70056a82db1ae49a2
srivb <basuvi4@gmail.com>
2026-07-10 19:16:35 -0400

    second  commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,5 @@
 public class Main {
-    public static void main() throws InterruptedException {
+    public static void main(String[] args) throws InterruptedException {
         for (int i = 0; i <5 ; i++) {
             System.out.println("Hello world");
             Thread.sleep(2000);
